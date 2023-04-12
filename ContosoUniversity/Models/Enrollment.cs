08357fbd781b05a4c3cbdcdc28696ef5fac71f37
uzoo0724 <kid07118@naver.com>
2023-04-12 15:40:45 +0900

    feat: Create a data model, Read related data

--- ContosoUniversity/Models/Enrollment.cs
+++ ContosoUniversity/Models/Enrollment.cs
@@ -1,14 +1,19 @@
-﻿namespace ContosoUniversity.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ContosoUniversity.Models
 {
     public enum Grade
     {
         A, B, C, D, E, F
     }
+
     public class Enrollment
     {
         public int EnrollmentID { get; set; }
         public int CourseID { get; set; }
         public int StudentID { get; set; }
+        [DisplayFormat(NullDisplayText = "No grade")]
         public Grade? Grade { get; set; }
 
         public Course Course { get; set; }
